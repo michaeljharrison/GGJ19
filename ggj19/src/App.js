af8524b31c77e53ca63321920a42a10e50340e9c
michaeljharrison <michael.j.harrison@outlook.com>
2019-01-19 20:37:06 +1100

    Initial Project Scaffolding.

--- ggj19/src/App.js
+++ ggj19/src/App.js
@@ -1,33 +1,13 @@
-import React, {
-  Component
-} from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Game } from './Pages';
 
 class App extends Component {
   render() {
-    return ( <
-      div className = "App" >
-      <
-      header className = "App-header" >
-      <
-      img src = {
-        logo
-      }
-      className = "App-logo"
-      alt = "logo" / >
-      <
-      p >
-      Edit < code > src / App.js < /code> and save TEST < /
-      p > <
-      a className = "App-link"
-      href = "https://reactjs.org"
-      target = "_blank"
-      rel = "noopener noreferrer" >
-      Learn React <
-      /a> < /
-      header > <
-      /div>
+    return (
+      <div className="App">
+        <Game />
+      </div>
     );
   }
 }
---
+++ ggj19/src/Pages/Game.jsx
@@ -0,0 +1,16 @@
+// @flow
+
+import React from 'react';
+
+type Props = {};
+type State = {};
+
+export default class Game extends React.Component<Props, State> {
+  render() {
+    return (
+      <div className="gamePageWrapper">
+        <span> Game Page.</span>
+      </div>
+    );
+  }
+}

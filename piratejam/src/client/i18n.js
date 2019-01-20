6966296a00767cfbc560be33392b7dc1ab46aff1
michaeljharrison <mike@southbanksoftware.com>
2019-01-21 07:10:33 +1100

    Formatting.

--- piratejam/src/client/i18n.js
+++ piratejam/src/client/i18n.js
@@ -1,18 +1,18 @@
 export const CONFIG = {
-    resources: {
-        en: {
-            translation: {
-                'Welcome to React': 'Welcome to React and react-i18next',
-                Input_Placeholder: 'Type your commands here.',
-            },
-        },
+  resources: {
+    en: {
+      translation: {
+        'Welcome to React': 'Welcome to React and react-i18next',
+        Input_Placeholder: 'Type your commands here.',
+      },
     },
-    lng: 'en',
-    fallbackLng: 'en',
+  },
+  lng: 'en',
+  fallbackLng: 'en',
 
-    interpolation: {
-        escapeValue: false,
-    },
+  interpolation: {
+    escapeValue: false,
+  },
 };
 
 export const STRINGS = {

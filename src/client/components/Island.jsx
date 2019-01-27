32166ec96e2cb2bd5f1884f9ef98b6fcfa500558
michaeljharrison <michael.j.harrison@outlook.com>
2019-01-27 11:38:23 +1100

    Ship IT

--- src/client/components/Island.jsx
+++ src/client/components/Island.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import IslandBG from '../style/assets/Island.png';
+import IslandBGEmpty from '../style/assets/Island_empty.png';
 // $FlowFixMe
 import './Island.scss';
 
@@ -33,7 +34,8 @@ export default class Island extends React.Component<Props, State> {
     if (enter) classes = 'enter';
     return (
       <div className={`Island overlay ${classes}`}>
-        <img className={`islandBG ${classes}`} src={IslandBG} alt="huh" />
+        {!leave && <img className={`islandBG ${classes}`} src={IslandBG} alt="huh" />}
+        {leave && <img className={`islandBG ${classes}`} src={IslandBGEmpty} alt="huh" />}
       </div>
     );
   }
